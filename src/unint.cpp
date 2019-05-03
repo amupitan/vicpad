a678ceaaa8878cbed1db33eb4588da6f149807f7
Victor Amupitan <amupitan@iastate.edu>
2019-05-03 11:09:45 -0500

    format unint

--- src/unint.cpp
+++ src/unint.cpp
@@ -1,53 +1,55 @@
 #include "../include/vicpad/unint.h"
 
 namespace utils {
-  unint::unint() : i(0){}
-   
-  unint::unint(int64_t n) {
-    i = (n < 0) ? 0 : n;
-  }
-  
-  unint::unint(const unint& n) : i(n.i){}
-  
-  unint& unint::operator--() {
-    if (i) i--;
-    return *this;
-  }
-  
-  unint unint::operator--(int) {
-    unint copy(*this);
-    if (i) i--;
-    return copy;
-  }
-  
-  unint& unint::operator++() {
-    i++;
-    return *this;
-  }
-  
-  unint unint::operator++(int) {
-    unint copy(*this);
-    i++;
-    return copy;
-  }
-  
-  // unint unint::operator+(const unint& n) const {
-  //   return unint(i + n.i);
-  // }
-  
-  // unint unint::operator-(const unint& n) const {
-  //   return unint(i - n.i);
-  // }
-  
-  // unint::operator int() const {
-  //   return i;
-  // }
-  
-  unint::operator uint64_t() const {
-    return i;
-  }
-  
-  uint64_t unint::value() const {
-    return i;
-  }
+unint::unint() : i(0) {}
+
+unint::unint(int64_t n) {
+  i = (n < 0) ? 0 : n;
 }
+
+unint::unint(const unint& n) : i(n.i) {}
+
+unint& unint::operator--() {
+  if (i)
+    i--;
+  return *this;
+}
+
+unint unint::operator--(int) {
+  unint copy(*this);
+  if (i)
+    i--;
+  return copy;
+}
+
+unint& unint::operator++() {
+  i++;
+  return *this;
+}
+
+unint unint::operator++(int) {
+  unint copy(*this);
+  i++;
+  return copy;
+}
+
+// unint unint::operator+(const unint& n) const {
+//   return unint(i + n.i);
+// }
+
+// unint unint::operator-(const unint& n) const {
+//   return unint(i - n.i);
+// }
+
+// unint::operator int() const {
+//   return i;
+// }
+
+unint::operator uint64_t() const {
+  return i;
+}
+
+uint64_t unint::value() const {
+  return i;
+}
+}  // namespace utils
